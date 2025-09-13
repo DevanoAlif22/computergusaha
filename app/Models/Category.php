2fd360e5d3c424fe0103cb2c26d6721795fdf97e
DevanoAlif22 <devanorama123@gmail.com>
2025-09-14 06:48:58 +0700

    fix : migration and model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-       protected $fillable = ['name'];
+    protected $table = 'categories';
+
+    protected $fillable = ['name'];
+
+    public function portofolios()
+    {
+        return $this->hasMany(Portofolio::class, 'category_id');
+    }
 }
